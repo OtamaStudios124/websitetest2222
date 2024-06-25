77037a9d01915c613e2e54919696014e11538309
OtamaStudios124 <ukanimatronicrepair@gmail.com>
2024-06-25 21:58:05 +0100

    bigbaby

--- main.js
+++ main.js
@@ -2,11 +2,15 @@ import './style.css'
 import { setupCounter } from './counter.js'
 
 document.querySelector('#app').innerHTML = `
-  <div>
-    <h1><iframe frameborder="0" src="https://itch.io/embed-upload/10627049?color=333333" allowfullscreen="" width="500" height="420"><a href="https://otama-studiosports.itch.io/otama-looks-at-the-mouse">Play Otama looks at the mouse on itch.io</a></iframe></h1>
+<h1>
+  <iframe frameborder="0" src="https://itch.io/embed-upload/10627049?color=333333" allowfullscreen="" width="500" height="420"></iframe>
+</h1>
+<p>Welcome to otama's free online games!</p>
+<p>Here you'll find a collection of games I have made myself,hopefully they are getting better with every release.</p>
+<p>I am slowly making new games, I am mostly focusing on small scale arcade games.</p>
+<p>If you're interested in putting some of these on your website,head down to the bottom and go to the info page. </p>
+<p>Enjoy the games!</p>
 
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+    
+`;
+
