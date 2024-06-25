b2211d86c2a5fb0596e5a0a7965be7a68d86779e
OtamaStudios124 <ukanimatronicrepair@gmail.com>
2024-06-25 21:16:57 +0100

    first updat

--- main.js
+++ main.js
@@ -3,14 +3,10 @@ import { setupCounter } from './counter.js'
 
 document.querySelector('#app').innerHTML = `
   <div>
-    <h1><iframe frameborder="0" src="https://itch.io/embed-upload/10618791?color=333333" allowfullscreen="" width="640" height="460"><a href="https://otama-studiosports.itch.io/cat-and-mouse">Play Cat And Mouse on itch.io</a></iframe></h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
+    <h1><iframe frameborder="0" src="https://itch.io/embed-upload/10627049?color=333333" allowfullscreen="" width="500" height="420"><a href="https://otama-studiosports.itch.io/otama-looks-at-the-mouse">Play Otama looks at the mouse on itch.io</a></iframe></h1>
+
     <p class="read-the-docs">
       Click on the Vite logo to learn more
     </p>
   </div>
 `
-
-setupCounter(document.querySelector('#counter'))
