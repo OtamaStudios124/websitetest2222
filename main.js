409c442eb2812a0c37e039b19a3b6a2321122f94
stewartcode <sdcowan@hotmail.com>
2024-06-26 23:31:02 +0100

    added working cutout

--- main.js
+++ main.js
@@ -1,86 +1,69 @@
 import "./style.css";
-import { setupCounter } from "./counter.js";
 
 document.querySelector("#app").innerHTML = `
 <body>
-  <h1><em>Welcome to Otama Free Online Games</em></h1>
-  <div id="iframe3" class="iframe-container">
-    <iframe frameborder="0" src="https://itch.io/embed-upload/10627049?color=333333" allowfullscreen="" width="500" height="420"><a href="https://otama-studiosports.itch.io/otama-looks-at-the-mouse">Play Otama looks at the mouse on itch.io</a></iframe>
-  </div>
-  <div>
-    <h1>Play with Otama ↑</h2>
-        <h2>Scroll down to view the games!</h2>
+    <h1><em>Welcome to Otama Free Online Games</em></h1>
+    <div>
+      <iframe frameborder="0" src="https://itch.io/embed-upload/10627049?color=333333" allowfullscreen="" width="500" height="420">
+        <a href="https://otama-studiosports.itch.io/otama-looks-at-the-mouse">Play Otama looks at the mouse on itch.io
+        </a>
+      </iframe>
+    </div>
+    <div>
+      <h1>Play with Otama ↑</h1>
+      <h2>Scroll down to view the games!</h2>
 
-    <p><strong>Welcome to otama's free online games! <br>Here you'll find a collection of games I have made myself, hopefully they are getting better with every release. <br>I am slowly making new games, I am mostly focusing on small scale arcade games.<br> If you're interested in putting some of these on your website, head down to the bottom and go to the info page.<br> </strong></p>
-     <h2 class="trigger" data-target="iframe3">Enjoy the games!</h2>
-     <h1>Games Menu:</h1>
-  </div>
-
-
-  <div class="grid-container">
-        <!-- 30 div elements for 3 columns and 10 rows -->
-        <div class="grid-item">Column 1, Row 1</div>
-        <div class="grid-item">Column 2, Row 1</div>
-        <div class="grid-item">Column 3, Row 1</div>
-        <div class="grid-item">Column 1, Row 2</div>
-        <div class="grid-item">Column 2, Row 2</div>
-        <div class="grid-item">Column 3, Row 2</div>
-        <div class="grid-item">Column 1, Row 3</div>
-        <div class="grid-item">Column 2, Row 3</div>
-        <div class="grid-item">Column 3, Row 3</div>
-        <div class="grid-item">Column 1, Row 4</div>
-        <div class="grid-item">Column 2, Row 4</div>
-        <div class="grid-item">Column 3, Row 4</div>
-        <div class="grid-item">Column 1, Row 5</div>
-        <div class="grid-item">Column 2, Row 5</div>
-        <div class="grid-item">Column 3, Row 5</div>
-        <div class="grid-item">Column 1, Row 6</div>
-        <div class="grid-item">Column 2, Row 6</div>
-        <div class="grid-item">Column 3, Row 6</div>
-        <div class="grid-item">Column 1, Row 7</div>
-        <div class="grid-item">Column 2, Row 7</div>
-        <div class="grid-item">Column 3, Row 7</div>
-        <div class="grid-item">Column 1, Row 8</div>
-        <div class="grid-item">Column 2, Row 8</div>
-        <div class="grid-item">Column 3, Row 8</div>
-        <div class="grid-item">Column 1, Row 9</div>
-        <div class="grid-item">Column 2, Row 9</div>
-        <div class="grid-item">Column 3, Row 9</div>
-        <div class="grid-item">Column 1, Row 10</div>
-        <div class="grid-item">Column 2, Row 10</div>
-        <div class="grid-item">Column 3, Row 10</div>
+      <p><strong>Welcome to otama's free online games! <br>Here you'll find a collection of games I have made myself, hopefully they are getting better with every release. <br>I am slowly making new games, I am mostly focusing on small scale arcade games.<br> If you're interested in putting some of these on your website, head down to the bottom and go to the info page.<br> </strong></p>
+      <h2 class="trigger" data-target="iframe3">Enjoy the games!</h2>
+      <h1>Games Menu:</h1>
     </div>
 
-
-  <div>
-    <h2 class="trigger" data-target="iframe1">Pacman 3D</h2>
-    <div id="iframe1" class="iframe-container">
-<iframe frameborder="0" src="https://itch.io/embed-upload/10658301?color=333333" allowfullscreen="" width="980" height="688"><a href="https://otama-studios.itch.io/pacman3d-updated-version">Play Pacman 3d (updated version) on itch.io</a></iframe>    </div>
-</div>
-
-<div>
-    <h2 class="trigger" data-target="iframe2">Wack-A-Pac</h2>
-    <div id="iframe2" class="iframe-container">
-      <iframe frameborder="0" src="https://itch.io/embed-upload/10641495?color=333333" allowfullscreen="" width="640" height="460"><a href="https://otama-studiosports.itch.io/wack-a-pac">Play Wack-A-Pac on itch.io</a></iframe>
+    <div id="iframe-container">
+      <iframe frameborder="0" src="https://itch.io/embed-upload/10641495?color=333333" allowfullscreen="" width="640" height="460">
+        <a href="https://otama-studiosports.itch.io">Play Wack-A-Pac on itch.io</a>
+      </iframe>
     </div>
-</div>
 
-
-</body>
-
-    
+    <div class="grid-container">
+          <div class="grid-item">
+            <h2 class="trigger" data-target="https://itch.io/embed-upload/10641495?color=333333">Pacman 3D</h2>
+          </div>
+          <div class="grid-item">
+            <h2 class="trigger" data-target="https://itch.io/embed-upload/10658301?color=333333">Wack-A-Pac</h2>
+          </div>
+          <div class="grid-item">
+            <h2 class="trigger" data-target="https://itch.io/embed-upload/10641495?color=333333">Pacman 3D</h2>
+          </div>
+          <div class="grid-item">
+            <h2 class="trigger" data-target="https://itch.io/embed-upload/10658301?color=333333">Wack-A-Pac</h2>
+          </div>
+          <div class="grid-item">
+            <h2 class="trigger" data-target="https://itch.io/embed-upload/10641495?color=333333">Pacman 3D</h2>
+          </div>
+          <div class="grid-item">
+            <h2 class="trigger" data-target="https://itch.io/embed-upload/10658301?color=333333">Wack-A-Pac</h2>
+          </div>
+          <div class="grid-item">
+            <h2 class="trigger" data-target="https://itch.io/embed-upload/10641495?color=333333">Pacman 3D</h2>
+          </div>
+          <div class="grid-item">
+            <h2 class="trigger" data-target="https://itch.io/embed-upload/10658301?color=333333">Wack-A-Pac</h2>
+          </div>
+          <div class="grid-item">
+            <h2 class="trigger" data-target="https://itch.io/embed-upload/10641495?color=333333">Pacman 3D</h2>
+          </div>
+          <div class="grid-item">
+            <h2 class="trigger" data-target="https://itch.io/embed-upload/10658301?color=333333">Wack-A-Pac</h2>
+          </div>
+    </div>
+</body> 
 `;
 
-document.addEventListener("click", function (event) {
+document.addEventListener("click", (event) => {
   if (event.target.classList.contains("trigger")) {
     const targetId = event.target.getAttribute("data-target");
-    const allIframes = document.querySelectorAll(".iframe-container");
-
-    allIframes.forEach((iframe) => {
-      iframe.classList.remove("active");
-    });
-
-    const targetIframe = document.getElementById(targetId);
-    targetIframe.classList.add("active");
+    const iframeContainer = document.getElementById("iframe-container");
+    const iframe = iframeContainer.querySelector("iframe");
+    iframe.src = targetId;
   }
 });
