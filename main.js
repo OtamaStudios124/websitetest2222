2e5dffa44cc0aaef7b29f2e13bd0f2d257b79a4e
OtamaStudios124 <ukanimatronicrepair@gmail.com>
2024-06-26 18:58:58 +0100

    ricky

--- main.js
+++ main.js
@@ -3,23 +3,26 @@ import { setupCounter } from './counter.js'
 
 document.querySelector('#app').innerHTML = `
 <body>
-  <h1>Welcome to Otama Free Online Games</h1>
+  <h1><em>Welcome to Otama Free Online Games</em></h1>
   <div>
   <iframe frameborder="0" src="https://itch.io/embed-upload/10627049?color=333333" allowfullscreen="" width="500" height="420"><a href="https://otama-studiosports.itch.io/otama-looks-at-the-mouse">Play Otama looks at the mouse on itch.io</a></iframe>
   </div>
   <div>
     <h1>Play with Otama ↑</h2>
-    <h2>Welcome to otama's free online games! Here you'll find a collection of games I have made myself, hopefully they are getting better with every release. I am slowly making new games, I am mostly focusing on small scale arcade games. If you're interested in putting some of these on your website, head down to the bottom and go to the info page. Enjoy the games!
+        <h2>Scroll down to view the games!</h2>
+
+    <p><strong>Welcome to otama's free online games! <br>Here you'll find a collection of games I have made myself, hopefully they are getting better with every release. <br>I am slowly making new games, I am mostly focusing on small scale arcade games.<br> If you're interested in putting some of these on your website, head down to the bottom and go to the info page.<br> </strong>
+<h2>Enjoy the games!</h2>
 
 
-
-</h2>
-    <h2>Scroll down to view the games!</h2>
+</p>
      <h1>Games Menu:</h1>
   </div>
   <div>
-    <h2>Pacman 3D</h2>
-    <iframe frameborder="0" src="https://itch.io/embed-upload/10658301?color=333333" allowfullscreen="" width="980" height="688"><a href="https://otama-studios.itch.io/pacman3d-updated-version">Play Pacman 3d (updated version) on itch.io</a></iframe>
+
+</>
+    <h2>3D Games: Pacman 3D, Frogs, Roller 3D, Snake 3D, Snake 1st Person, Crazy Mazy</h2>
+    <h2>2D Games:Wack-A-Pac, Pac-Catch, Space Invaders, Ms Pacman, Sukoban, Frogger</h2>
   </div>
 </body>
 
