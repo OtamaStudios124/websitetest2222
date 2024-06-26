798db73851f8a5db5aef33ed85fe936b8b51479b
stewartcode <sdcowan@hotmail.com>
2024-06-26 20:12:46 +0100

    added new click event

--- main.js
+++ main.js
@@ -4,15 +4,15 @@ import { setupCounter } from './counter.js'
 document.querySelector('#app').innerHTML = `
 <body>
   <h1><em>Welcome to Otama Free Online Games</em></h1>
-  <div>
-  <iframe frameborder="0" src="https://itch.io/embed-upload/10627049?color=333333" allowfullscreen="" width="500" height="420"><a href="https://otama-studiosports.itch.io/otama-looks-at-the-mouse">Play Otama looks at the mouse on itch.io</a></iframe>
+  <div id="iframe3" class="iframe-container">
+    <iframe frameborder="0" src="https://itch.io/embed-upload/10627049?color=333333" allowfullscreen="" width="500" height="420"><a href="https://otama-studiosports.itch.io/otama-looks-at-the-mouse">Play Otama looks at the mouse on itch.io</a></iframe>
   </div>
   <div>
     <h1>Play with Otama ↑</h2>
         <h2>Scroll down to view the games!</h2>
 
     <p><strong>Welcome to otama's free online games! <br>Here you'll find a collection of games I have made myself, hopefully they are getting better with every release. <br>I am slowly making new games, I am mostly focusing on small scale arcade games.<br> If you're interested in putting some of these on your website, head down to the bottom and go to the info page.<br> </strong>
-<h2>Enjoy the games!</h2>
+<h2 class="trigger" data-target="iframe3">Enjoy the games!</h2>
 
 
 </p>
