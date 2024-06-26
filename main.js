6ad3e66f887d005ea1c3ca60cecb0ee0815d67e8
OtamaStudios124 <ukanimatronicrepair@gmail.com>
2024-06-26 17:50:11 +0100

    firstbestversion

--- main.js
+++ main.js
@@ -8,8 +8,13 @@ document.querySelector('#app').innerHTML = `
   <iframe frameborder="0" src="https://itch.io/embed-upload/10627049?color=333333" allowfullscreen="" width="500" height="420"><a href="https://otama-studiosports.itch.io/otama-looks-at-the-mouse">Play Otama looks at the mouse on itch.io</a></iframe>
   </div>
   <div>
-    <h2>Play with Otama ↑</h2>
-    <h3>Scroll down to view the games!</h3>
+    <h1>Play with Otama ↑</h2>
+    <h2>Welcome to otama's free online games! Here you'll find a collection of games I have made myself, hopefully they are getting better with every release. I am slowly making new games, I am mostly focusing on small scale arcade games. If you're interested in putting some of these on your website, head down to the bottom and go to the info page. Enjoy the games!
+
+
+
+</h2>
+    <h2>Scroll down to view the games!</h2>
      <h1>Games Menu:</h1>
   </div>
   <div>
