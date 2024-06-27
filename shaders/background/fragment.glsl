722644cf77e3562cf4191e6dad4f372479d1ca7a
OtamaStudios124 <ukanimatronicrepair@gmail.com>
2024-06-27 19:34:06 +0100

    name

--- shaders/background/fragment.glsl
+++ shaders/background/fragment.glsl
@@ -6,7 +6,7 @@ void main() {
     float distance = length(vUv - center);
     
     // Create color bands
-    vec3 color = vec3(0.0, 0.3, 0.7); // Base blue color
+    vec3 color = vec3(1.0, 0.27, 0.27); // Base blue color
     color += vec3(0.1, 0.2, 0.3) * sin(distance * 10.0 - time * 2.0);
     
     // Add a softer, less bright glow at the center
@@ -17,4 +17,5 @@ void main() {
     color = clamp(color, 0.0, 1.0);
 
     gl_FragColor = vec4(color, 1.0);
+    //gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
 }
